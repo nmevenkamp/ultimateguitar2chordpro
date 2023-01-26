abb17f3f3e2974daf4ee086dd48a97d0e10b34b6
nmevenkamp <niklas.mevenkamp@gmail.com>
2023-01-26 21:46:20 +0100

    improve compatibility

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import os
 import re
 import unicodedata
+from enum import Enum
 
 
+class OutputMode(Enum):
+    PRINT = 0
+    SAVE = 1
+
+
+MODE = OutputMode.SAVE
 INPUT_FILE = "plain_text.txt"
 OUTPUT_DIR = "D:\\Dropbox\\ChordPro"
-TITLE = "The Call and the Answer"
-ARTIST = "De Dannan"
-CAPO = 3
+TITLE = "Read My Mind"
+ARTIST = "The Killers"
+CAPO = 1
 
 
 def slugify(value, allow_unicode=False):
@@ -47,6 +54,10 @@ def get_chords_positions(chords_str):
 
 def merge(line, chords_str):
     chords, positions = get_chords_positions(chords_str)
+    if len(line) - 1 < positions[-1]:
+        offset = positions[-1] - len(line) + 1
+        line = line.replace("\n", "")
+        line += "".join([" "] * offset) + "\n"
     res = ""
     for i, c in enumerate(line):
         if i in positions:
@@ -59,7 +70,7 @@ def merge(line, chords_str):
 
 
 def main():
-    with open(INPUT_FILE, 'r') as f:
+    with open(INPUT_FILE, 'r', encoding='utf-8') as f:
         plain_text = f.readlines()
 
     res = f"{{title: {ARTIST} - {TITLE}}}\r\n"
@@ -72,6 +83,10 @@ def main():
             i += 1
             res += "\r\n"
             continue
+        if plain_text[i][0] == ";":
+            res += merge("", plain_text[i][1:])
+            i += 1
+            continue
         chords_str = plain_text[i]
         line = plain_text[i+1]
         res += merge(line, chords_str)
@@ -79,8 +94,12 @@ def main():
 
     basename = f"{slugify(ARTIST)}-{slugify(TITLE)}"
     fname = os.path.join(OUTPUT_DIR, f"{basename}.cho")
-    with open(fname, 'w') as f:
-        f.write(res)
+
+    if MODE is OutputMode.PRINT:
+        print(res)
+    elif MODE is OutputMode.SAVE:
+        with open(fname, 'w') as f:
+            f.write(res)
 
 
 if __name__ == "__main__":
